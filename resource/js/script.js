c8ba0712de3050b0a15a09b11b6867ae9f1095d1
Angga Kusuma <manggakusuma810@gmail.com>
2022-12-16 02:11:37 +0700

    fix2

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,60 +1,60 @@
-// "use strict";
+"use strict";
 
-// let datas = [
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/map_of_the_soul-7/img01.jpg`,
-//     title: "MAP OF THE SOUL : 7",
-//     desc: `BTS Releases 4th Studio Album “MAP OF THE SOUL : 7”!
-//     A heartfelt story of BTS and their search for their “real selves”
+let datas = [
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/map_of_the_soul-7/img01.jpg`,
+    title: "MAP OF THE SOUL : 7",
+    desc: `BTS Releases 4th Studio Album “MAP OF THE SOUL : 7”!
+    A heartfelt story of BTS and their search for their “real selves”
     
-//     BTS will release their fourth studio album “MAP OF THE SOUL : 7” on February 21.`,
-//   },
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/proof/discography-cover.png`,
-//     title: "WE ARE BULLETPROOF",
-//     desc: `BTS released the new anthology album Proof on June 10, 11 months after their latest single CD Butter. Proof is the core of BTS’ history. The new three-CD album introduces three new tracks, one in each CD: “Yet To Come (The Most Beautiful Moment)”, “Run BTS” and “For Youth”, each in different genres conveying their own messages.
+    BTS will release their fourth studio album “MAP OF THE SOUL : 7” on February 21.`,
+  },
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/proof/discography-cover.png`,
+    title: "WE ARE BULLETPROOF",
+    desc: `BTS released the new anthology album Proof on June 10, 11 months after their latest single CD Butter. Proof is the core of BTS’ history. The new three-CD album introduces three new tracks, one in each CD: “Yet To Come (The Most Beautiful Moment)”, “Run BTS” and “For Youth”, each in different genres conveying their own messages.
 
-//     The album comprises 48 tracks total, from the greatest hit songs, and solo/sub-unit tracks selected by each member, unreleased tracks to special tracks.`,
-//   },
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/young_forever/album-cover.jpg`,
-//     title: "THE MOST BEAUTIFUL MOMENT IN LIFE : YOUNG FOREVER",
-//     desc: `The final chapter of the story of The most beautiful moment in life
+    The album comprises 48 tracks total, from the greatest hit songs, and solo/sub-unit tracks selected by each member, unreleased tracks to special tracks.`,
+  },
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/young_forever/album-cover.jpg`,
+    title: "THE MOST BEAUTIFUL MOMENT IN LIFE : YOUNG FOREVER",
+    desc: `The final chapter of the story of The most beautiful moment in life
 
-//     Having won over the world with their two-part The most beautiful moment in life series in 2015`,
-//   },
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/you_never_walk_alone/album-cover.jpg`,
-//     title: "YOU NEVER WALK ALONE",
-//     desc: `BTS releases YOU NEVER WALK ALONE!
-//     The epilogue brings closure to WINGS.
-//     “We can laugh if we are together”, a message of comfort and hope to the youth of this generation.`,
-//   },
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/love_yourself-tear/album-cover.jpg`,
-//     title: "LOVE YOURSELF轉 ‘TEAR’",
-//     desc: `BTS releases third studio album LOVE YOURSELF 轉 ‘TEAR’
-//     Fake love inevitably leads to loss and separation,
-//     For true love can only begin when you learn to love yourself`,
-//   },
-//   {
-//     imgSrc: `https://ibighit.com/bts/images/bts/discography/map_of_the_soul-persona/album-cover.jpg`,
-//     title: "MAP OF THE SOUL : PERSONA",
-//     desc: `BTS’ MAP OF THE SOUL : PERSONA
-//     GLOBAL ICON BTS POURS OUT THEIR HEARTS TO FANS
-//     FINDING JOY IN LOVE AND REACHING OUT TO THE WORLD THROUGH THE LEAD SINGLE ‘Boy With Luv (Feat. Halsey)’
-//     `,
-//   },
-// ];
+    Having won over the world with their two-part The most beautiful moment in life series in 2015`,
+  },
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/you_never_walk_alone/album-cover.jpg`,
+    title: "YOU NEVER WALK ALONE",
+    desc: `BTS releases YOU NEVER WALK ALONE!
+    The epilogue brings closure to WINGS.
+    “We can laugh if we are together”, a message of comfort and hope to the youth of this generation.`,
+  },
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/love_yourself-tear/album-cover.jpg`,
+    title: "LOVE YOURSELF轉 ‘TEAR’",
+    desc: `BTS releases third studio album LOVE YOURSELF 轉 ‘TEAR’
+    Fake love inevitably leads to loss and separation,
+    For true love can only begin when you learn to love yourself`,
+  },
+  {
+    imgSrc: `https://ibighit.com/bts/images/bts/discography/map_of_the_soul-persona/album-cover.jpg`,
+    title: "MAP OF THE SOUL : PERSONA",
+    desc: `BTS’ MAP OF THE SOUL : PERSONA
+    GLOBAL ICON BTS POURS OUT THEIR HEARTS TO FANS
+    FINDING JOY IN LOVE AND REACHING OUT TO THE WORLD THROUGH THE LEAD SINGLE ‘Boy With Luv (Feat. Halsey)’
+    `,
+  },
+];
 
-// let listAlbums = document.querySelector("#list-albums");
+let listAlbums = document.querySelector("#list-albums");
 
-// datas.forEach((item) => {
-//   listAlbums.innerHTML += `<div class="card">
-//               <img src=${item.imgSrc} class="card-img-top" alt="Albums">
-//               <div class="card-body">
-//                   <a href="#" class="card-title"><h4>${item.title}</h4></a>
-//                   <p class="card-text ellipsis">${item.desc}</p>
-//               </div>
-//             </div>`;
-// });
+datas.forEach((item) => {
+  listAlbums.innerHTML += `<div class="card">
+              <img src=${item.imgSrc} class="card-img-top" alt="Albums">
+              <div class="card-body">
+                  <a href="#" class="card-title"><h4>${item.title}</h4></a>
+                  <p class="card-text ellipsis">${item.desc}</p>
+              </div>
+            </div>`;
+});
